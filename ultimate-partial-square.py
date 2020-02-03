b75528d97d0d38f1bbd9aeb9c92a2b3861bbc56c
Angelo Antonio Salatino <angelosalatino@users.noreply.github.com>
2020-02-03 14:57:13 +0000

    Update ultimate-partial-square.py

--- ultimate-partial-square.py
+++ ultimate-partial-square.py
@@ -13,41 +13,38 @@ def ultimate_partial_square():
     
     while current_length < limit:
         
+        #prints progress
         if len(digits) % 1000 == 0:
             print("I have reached this", len(digits))
-        # will keep the result number in vector 
-        # in reverse order 
         
-        result = [0] + digits#* (current_length + 1) 
-        #total_len = len(result)
+        result = [0] + digits #we just need to compute result[0] which is set to 0 at the moment
 
         
         carry_new = 0
         value_a = 0
+        
         for i in range(0, current_length // 2):
             j = current_length-i-2
             temp_a = digits[i] * digits[j]
             if i!=j:
                 temp_a += temp_a
             value_a += temp_a
-            #carry_new += temp_a // 10
-            #value_a += temp_a % 10
                 
-        carry_new = value_a // 10
-        value_a = value_a % 10
-        #print(current_length,":",value_a)
+        carry_new = value_a // 10 # green part of the formula
+        value_a = value_a % 10 # blue part of the formula
         
+        #orange part of the formula
         if current_length > 1:
             value_b = (digits[0] * digits[current_length-1]) // 5
-        else:
+        else: #exception for t00
             value_b = (digits[0] * digits[current_length-1]) // 10
 
         
         t_result = value_a + value_b + carry_old 
-        result[0] = t_result % 10
-        carry_result = t_result // 10
+        result[0] = t_result % 10 #value x_n+1
+        carry_result = t_result // 10 #red part of the formula
         
-        carry_old = carry_new + carry_result
+        carry_old = carry_new + carry_result # carry for the next iteration
         digits = result
         current_length = len(digits)
     
